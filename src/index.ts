fccf30ca0c8c3dfd8f267fbd9499b767a8aa0865
Claudio Cortese <claudio.cortese@outlook.it>
2019-12-25 21:10:08 +0100

    fix: remove jsonattributes default params

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
-import axios from 'axios';
-
 export { default as configureAuthentication } from './configureAuthentication';
 export { default as useForwardGeocoding } from './geocoding/useForwardGeocoding';
 export { default as useReverseGeocoding } from './geocoding/useReverseGeocoding';
-
-axios.defaults.params = {
-  jsonattributes: 1,
-};
