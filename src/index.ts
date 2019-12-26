7a38079c0f781aecd64cfe295b0c996dc56fd79e
Claudio Cortese <claudio.cortese@outlook.it>
2019-12-26 02:14:57 +0100

    fix: export useWeather hook

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as configureAuthentication } from './configureAuthentication';
 export { default as useForwardGeocoding } from './geocoding/useForwardGeocoding';
 export { default as useReverseGeocoding } from './geocoding/useReverseGeocoding';
+export { default as useWeather } from './useWeather';
